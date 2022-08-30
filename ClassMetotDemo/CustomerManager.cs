a652ad4a5be104f2e733e8ca1197a20d588c2d76
iyilm <iyilm@ISMAILYILMAZER>
2022-08-30 10:41:03 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.name + " adli kullanici eklendi ");
+
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.name + " adli kullanici silindi");
+
+        }
+
+        public void List(params Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("kullanıcilar listeleniyor....");
+                Console.WriteLine(customer.name + " adli kullanici listede ");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.name = "christian ozera";
+            customer1.job = "engineer";
+            customer1.id = 1234;
+
+            Customer customer2 = new Customer();
+            customer2.name = "Mad max";
+            customer2.id = 1436;
+            customer2.job = "driver";
+
+            Customer customer3 = new Customer();
+            customer3.name = "carl johanson";
+            customer3.id = 35447;
+            customer3.job = "police";
+
+            CustomerManager operation=new CustomerManager();
+            operation.Add(customer1);
+            operation.Delete(customer2);
+            operation.List(customer1, customer2,customer3);
+
+        }
+    }
+}
